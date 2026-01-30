441aa419d164c22ba8a6b76e9ad1b26a2b72bdf3
haooab <huihao@ualberta.ca>
2026-01-30 17:25:47 -0400

    change color

--- app/src/main/java/com/example/wecook/Shape.java
+++ app/src/main/java/com/example/wecook/Shape.java
@@ -2,7 +2,7 @@ package com.example.wecook;
 
 public abstract class Shape {
 
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "$Blue";
     private int x;
     private int y;
 }
